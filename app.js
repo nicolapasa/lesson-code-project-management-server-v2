e4e6022e3e1395572b0e636ca23fce06effb9459
nicolapasa <nicola.pasa@gmail.com>
2023-08-02 16:21:46 +0200

    authentication token

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require("./db");
 const express = require("express");
 
 const app = express();
-
+const { isAuthenticated } = require("./middlewares/jwt.middleware"); // <== IMPORT
 require("./config")(app);
 
 // 👇 Start handling routes here
@@ -11,10 +11,14 @@ const allRoutes = require("./routes");
 app.use("/api", allRoutes);
 
 const projectRouter = require("./routes/project.routes");
-app.use("/api", projectRouter);
+app.use("/api", isAuthenticated, projectRouter);  
 
 const taskRouter = require("./routes/task.routes");
-app.use("/api", taskRouter);
+app.use("/api", isAuthenticated, taskRouter);            // <== UPDATE
+
+const authRouter = require("./routes/auth.routes");       //  <== IMPORT
+app.use("/auth", authRouter);       
+
 
 require("./error-handling")(app);
 
